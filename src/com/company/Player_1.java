189e0ece8865ce8b137fa8daab3d14f9dd1e9aa3
sam.richard <sam.richard@digipen.edu>
2017-07-19 11:20:09 -0700

    Initial Commit

--- src/com/company/Player_1.java
+++ src/com/company/Player_1.java
@@ -3,7 +3,6 @@ package com.company;
 import edu.digipen.InputManager;
 import edu.digipen.gameobject.GameObject;
 import edu.digipen.gameobject.ObjectManager;
-import edu.digipen.level.GameLevelManager;
 import edu.digipen.math.Vec2;
 
 import java.awt.event.KeyEvent;
@@ -12,81 +11,79 @@ import java.awt.event.KeyEvent;
  * Created by sam.richard on 7/18/2017.
  */
 import edu.digipen.math.Vec2;
-public class Player_1 extends GameObject {
-    public Vec2 direction = new Vec2(1.0f, 0.0f);
-
-    public Player_1() {
-        super("Player_1", 38, 29, "Omana.png");
-        setRectangleCollider(19, 14.5f);
+public class Player_1 extends GameObject{
+    public Vec2 direction= new Vec2(1.0f,0.0f);
+    public Player_1()
+    {
+        super ("Player_1",38,29,"Omana.png");
+        setRectangleCollider(19,14.5f);
     }
+    private void shoot()
+    {
 
-    private void shoot() {
 
 
         ObjectManager.addGameObject(new Bullet(direction, getPosition()));
     }
 
-    @Override
-    public void update(float dt) {
-        if (InputManager.isPressed(KeyEvent.VK_SPACE)) {
+    @ Override
+    public void update(float dt)
+    {
+
+        float movementSpeed= 3.0f;
+        float rotationSpeed= 1.0f;
+
+        if(InputManager.isPressed(KeyEvent.VK_UP))
+        {
+            setRotation(0);
+            direction.set(0.0f, 1.0f);
             shoot();
         }
-        float movementSpeed = 3.0f;
-        float rotationSpeed = 1.0f;
+        if(InputManager.isPressed(KeyEvent.VK_DOWN))
+        {
+            setRotation(180);
+            direction.set(0.0f, -1.0f);
+            shoot();
+        }
+        if(InputManager.isPressed(KeyEvent.VK_LEFT))
+        {
+            setRotation(270);
+            direction.set(-1.0f, 0.0f);
+            shoot();
+        }
+        if(InputManager.isPressed(KeyEvent.VK_RIGHT))
+        {
+            setRotation(90);
+            direction.set(1.0f, 0.0f);
+            shoot();
+        }
 
-        /**if(InputManager.isPressed(KeyEvent.VK_A))
-         {
-         float rotation= getRotation();
-         rotation+= rotationSpeed;
-         setRotation(rotation);
-         rotation= (float)Math.toRadians(rotation);
-         direction.setX((float)Math.cos(rotation));
-         direction.setY((float) Math.sin(rotation));
-
-         }
-         if(InputManager.isPressed(KeyEvent.VK_D))
-         {
-         float rotation= getRotation();
-         rotation-= rotationSpeed;
-         setRotation(rotation);
-         rotation= (float)Math.toRadians(rotation);
-         direction.setX((float)Math.cos(rotation));
-         direction.setY((float) Math.sin(rotation));
-         }
-         if(InputManager.isPressed(KeyEvent.VK_W))
-         {
-         Vec2 position= getPosition();
-         position.add(Vec2.scale(direction, movementSpeed));
-         setPosition(position);
-         }
-         if(InputManager.isPressed(KeyEvent.VK_S))
-         {
-         Vec2 position= getPosition();
-         position.subtract(Vec2.scale(direction,movementSpeed));
-         setPosition(position);
-         }*/
-
-        if (InputManager.isPressed(KeyEvent.VK_W)) {
+        if(InputManager.isPressed(KeyEvent.VK_W))
+        {
             float y = getPositionY();
-            y += 4;
+            y +=4;
             setPositionY(y);
         }
-        if (InputManager.isPressed(KeyEvent.VK_S)) {
+        if(InputManager.isPressed(KeyEvent.VK_S))
+        {
             float y = getPositionY();
-            y += -4;
+            y +=-4;
             setPositionY(y);
         }
-        if (InputManager.isPressed(KeyEvent.VK_A)) {
+        if(InputManager.isPressed(KeyEvent.VK_A))
+        {
             float x = getPositionX();
-            x += -4;
+            x +=-4;
             setPositionX(x);
         }
-        if (InputManager.isPressed(KeyEvent.VK_D)) {
+        if(InputManager.isPressed(KeyEvent.VK_D))
+        {
             float x = getPositionX();
-            x += 4;
+            x +=4;
             setPositionX(x);
         }
     }
+
 
     @Override
     public void collisionReaction(GameObject collidedWith)
