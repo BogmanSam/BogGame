c4af18d5cf59225b2535f37f8d30ab39543e1d75
sam.richard <sam.richard@digipen.edu>
2017-07-20 09:08:26 -0700

    Initial Commit

--- src/com/company/Bullet.java
+++ src/com/company/Bullet.java
@@ -13,7 +13,7 @@ public class Bullet extends GameObject
 {
     public Bullet()
     {
-        super("Bullet", 5, 5, "ball.png");
+        super("Bullet", 2, 2, "ball.png");
 
     }
     public Vec2 direction;
@@ -23,14 +23,14 @@ public class Bullet extends GameObject
     }
     public Bullet(Vec2 direction_, Vec2 location)
     {
-        super("Bullet",5,5,"ball.png");
+        super("Bullet",2,2,"ball.png");
         direction = new Vec2();
         direction.setX(direction_.getX());
         direction.setY(direction_.getY());
         setScaleX(3.0f);
         setScaleY(7.0f);
         setPosition(location);
-        setRectangleCollider(2.5f, 2.5f);
+        setRectangleCollider(1f, 1f);
     }
 
     @Override
