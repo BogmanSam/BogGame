3826304dad93ca94be50a8860140eebc6b7e95e1
sam.richard <sam.richard@digipen.edu>
2017-07-21 11:24:12 -0700

    Initial Commit

--- src/com/company/MainMenu.java
+++ src/com/company/MainMenu.java
@@ -16,13 +16,9 @@ public class MainMenu extends GameLevel {
     @Override
     public void create()
     {
-        GameObject startText = new GameObject("StartText", 400, 100, "StartText.png");
-        startText.setPositionY(100);
+        GameObject startText = new GameObject("MainBackground", 900, 900, "MainMen.png");
         ObjectManager.addGameObject(startText);
 
-        GameObject quitText = new GameObject("QuitText", 400, 100, "QuitText.png");
-        quitText.setPositionY(-100);
-        ObjectManager.addGameObject(quitText);
     }
 
     @Override
